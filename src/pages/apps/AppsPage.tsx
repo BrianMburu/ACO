7abd96e949bdf844b174a9c15173fbd4d012751c
Brian Mburu <brian.mburu@students.jkuat.ac.ke>
2024-10-12 18:59:28 +0300

    Removed api key from aoclimaoptions component.

--- src/pages/apps/AppsPage.tsx
+++ src/pages/apps/AppsPage.tsx
@@ -53,7 +53,7 @@ interface HistoricalData {
 const AppsPage: React.FC = () => {
   const navigate = useNavigate();
 
-  const api_key = "a2f4db644e9107746535b0d2ca43b85d";
+  const api_key = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
   if (!api_key) {
     throw new Error(`No OpenWeather API key Found in environment`);
